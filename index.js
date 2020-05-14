fd6f0f0333b3b6c4a02ae0521f3bf2fbc4d30c3c
amine.tchitooss <amine.tchitooss@gmail.com>
2020-05-14 17:47:03 +0200

    addtext feature

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const nodeMailer = require('nodemailer');
-const xoauth2 = require('xoauth2');
 const bodyParser = require('body-parser');
 const express = require('express');
 const exphbs = require('express-handlebars');
@@ -9,8 +8,11 @@ var cors = require('cors');
 var fetch = require('fetch').fetchUrl;
 require('dotenv').config();
 const port = process.env.PORT || 3000
-const app = express();
+const accountSsId = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const client = require('twilio')(accountSsId, authToken)
 
+const app = express();
 
 // View engine setup
 app.engine('handlebars', exphbs({
@@ -23,26 +25,26 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 //middle ware config
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// const allowedOrigins = [
-//     'https://tchitosmailer.herokuapp.com'
-//     , 'http://localhost:' + port
-// ];
+const allowedOrigins = [
+    'https://tchitosmailer.herokuapp.com'
+    , 'http://localhost:' + port
+];
 
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         // allow requests with no origin (like mobile apps or curl requests)
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) {
-//             var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.listen(port, () => console.log('server running on' + port))
 
@@ -51,13 +53,6 @@ const transporter = nodeMailer.createTransport({
     auth: {
         user: process.env.MAIL,
         pass: process.env.PASS,
-        // xoauth2: xoauth2.createXOAuth2Generator({
-        //     user: process.env.MAIL,
-        //     clientId: process.env.CLIENT_ID,
-        //     clientSecret: process.env.CLIENT_SECRET,
-        //     refreshToken: process.env.REFRESH_TOKEN,
-        //     // accessToken: process.env.ACCESS_TOKEN
-        // })
     },
     tls: {
         rejectUnauthorized: false
@@ -83,8 +78,27 @@ app.post('/sendMail', async (req, res) => {
     const response = await sendMail(mailOutput)
     res.end(JSON.stringify(response))
 })
-app.post('/sendText', (req, res) => {
 
+app.post('/sendText', async (req, res) => {
+
+    //simple protection so that nobody uses the API
+    // (it's a shortcut) still a token is the best way to go
+    if (req.body.cipher !== process.env.TWILIO_CIPHER)
+        return res.status(403).send('cipher error')
+
+    const { error } = validateTextData(req.body)
+    if (error) { return res.status(403).send(error.details[0].message) }
+
+    const vText = {
+        from: process.env.TWILIO_NUMBER,
+        to: req.body.to, //process.env.TWILIO_TST_NUMBER (registred numbers only in trial)
+        body: req.body.body
+    }
+    return client.messages.create(vText).then(response => {
+        return res.render('sent', { msg: 'Text has been sent' });
+    }).catch(err => {
+        return res.end(err.message)
+    });
 })
 
 app.post('/sendMailForm', (req, res) => {
@@ -115,13 +129,13 @@ app.post('/sendMailForm', (req, res) => {
         if (response == "ok")
             return res.render('sent', { msg: 'Email has been sent' });
         else {
-            return res.end(response)
+            return res.end(JSON.parse(response))
         }
     })
     //  res.end(JSON.stringify(response))
 });
 
-function sendMail(pMail, pForm) {
+function sendMail(pMail) {
     return new Promise((resolve, reject) => {
         transporter.sendMail(pMail, (err, info) => {
             if (err) {
@@ -133,6 +147,7 @@ function sendMail(pMail, pForm) {
         });
     });
 }
+
 function getIp() {
     return new Promise((resolve, reject) => {
         fetch('https://api.ipify.org/?format=json', (error, meta, body) => {
@@ -149,10 +164,20 @@ function validateMailData(pData) {
     })
     return schema.validate(pData)
 }
+
 function validateMailDataForm(pData) {
     const schema = Joi.object({
         email: Joi.string().email().required(),
         message: Joi.string().required()
+    })
+    return schema.validate(pData)
+}
+
+function validateTextData(pData) {
+    const schema = Joi.object({
+        cipher: Joi.string(),
+        to: Joi.string().regex(/^[+][1-9]{1,3}\d{9}$/).required(),
+        body: Joi.string().required()
     })
     return schema.validate(pData)
 }
